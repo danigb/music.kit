964ea4e03a4ba2709b920a429b9db11cc2dd2783
danigb <danigb@gmail.com>
2015-10-25 16:16:12 +0100

    Add more modules. Publish a distribution file

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ var music = {}
 
 music.notation = require('music.notation')
 music.note = require('music.note.height')
+music.note.transpose = require('music.note.transpose')
 // music.note.enharmonics = require('music.note.enharmonics')
-// music.transpose = require('music.transpose')
 // music.interval = require('music.interval')
 music.gamut = require('music.gamut')
 music.pitchSet = require('music.pitchset')
 music.scale = require('music.scale')
+music.scale.degrees = require('music.scale.degrees')
+music.scales = require('music.scales')
 music.chord = require('music.chord')
 
-module.exports = music
+if (typeof module === 'object' && module.exports) module.exports = music
+if (typeof window !== 'undefined') window.music = music
